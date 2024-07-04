b339d92df0d04d49e4e698a9a11b202b6f26f9d2
shiron <shiron4710.dev@gmail.com>
2024-07-05 03:55:34 +0900

    ci: fix lighthouse

---
+++ .github/scripts/lighthouse.js
@@ -0,0 +1,21 @@
+/**
+ * 
+ * @param {{url: string, summary: Record<string, number>}[]} manifest 
+ */
+module.exports = (manifest) => {
+  manifest.map((result) => {
+    const summary = result.summary;
+    const kv = Object.entries(summary).map(([key, value]) => {
+      const score = value * 100;
+      const emoji = score >= 90 ? "🟢" : score >= 50 ? "🟠" : "🔴";
+      return { key: `${emoji} ${score}`};
+    });
+    // to markdown table
+    const header = "|" + Object.keys(kv).join("|") + "|";
+    const row = "|" + Object.values(kv).join("|") + "|";
+    const output = header + "\n" +
+      "|" + kv.map(() => "---").join("|") + "|" + "\n" +
+      row;
+    console.log(output);
+  });
+}
