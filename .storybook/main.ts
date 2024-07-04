d33caa8244d9ef7ac6a5978da9d036c2b53049a6
shiron-dev <shiron4710.dev@gmail.com>
2024-07-04 15:59:35 +0900

    chore: storybook (#23)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,20 +1,17 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  stories: [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
-  ],
+  stories: ["../app/**/*.stories.@(ts|tsx)"],
   addons: [
     "@storybook/addon-onboarding",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
     "@storybook/addon-interactions",
   ],
   framework: {
     name: "@storybook/react-vite",
     options: {},
   },
+  staticDirs: ["../public"],
 };
 export default config;
