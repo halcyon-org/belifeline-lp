08fbe91d3428c99b5dae9c02776a2a28b0f6ac0f
Souma <102800255+6ouma@users.noreply.github.com>
2024-07-05 10:26:11 +0900

    feat(page): Add second page (#41)
    Describe what and how this service provides.

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -2,15 +2,17 @@ import { Header } from "@/components/Header";
 import { ReactNode } from "react";
 import { Hero } from "./features/Hero/Hero";
 import { Page1 } from "./features/Page1/Page1";
+import { Page2 } from "./features/Page2/Page2";
 import { Footer } from "@/components/Footer";
 
 export default function Page(): ReactNode {
   return (
     <>
       <Header />
-      <main>  
+      <main>
         <Hero />
         <Page1 />
+        <Page2 />
       </main>
       <Footer />
     </>
