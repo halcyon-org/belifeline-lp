41a3a6e401a58709b94a95719574f50fe945f586
r03i22 <103725932+r03i22@users.noreply.github.com>
2024-07-11 16:09:25 +0900

    feat: update footer (#55)
    * feat: update footer
    
    * feat: #43 up date
    
    * Update app/components/Footer/styles.css.ts
    
    Co-authored-by: shiron-dev <shiron4710.dev@gmail.com>
    
    * style: update
    
    * style: update2
    
    ---------
    
    Co-authored-by: shiron-dev <shiron4710.dev@gmail.com>

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -8,12 +8,17 @@ export function Footer(): ReactNode {
             <li>
               <a className={styles.menuItem}>プライバシーポリシー</a>
             </li>
+            <span className={styles.divider}></span>
             <li>
               <a className={styles.menuItem}>利用規約</a>
             </li>
+            <span className={styles.divider}></span>
             <li>
               <a className={styles.menuItem}>お問い合わせ</a>
             </li>
+          </ul>
+          <ul>
+            <a className={styles.orgText}>Halcyon</a>
           </ul>
     </footer>
   );
--- app/components/Footer/styles.css.ts
+++ app/components/Footer/styles.css.ts
@@ -4,26 +4,59 @@ import { style } from "@vanilla-extract/css";
 export const footer = style({
   display: "flex",
   padding: "0.625rem 6.25rem",
-  justifyContent: "space-between",
-  alignItems: "flex-start",
+  justifyContent: "center",
+  textAlign: "center",
+  alignItems: "center",
   alignSelf: "stretch",
+  position: "relative",
   background: vars.color.footerBg,
+  color: vars.color.lightText,
+});
+
+export const menuContainer = style({
+  display: "flex",
+  alignItems: "center",
 });
 
 export const menu = style({
-  display: "flex",
-  padding: "0rem 1.25rem",
+  display: "inline-flex",
   margin: 0,
   justifyContent: "center",
-  alignItems: "flex-start",
+  alignItems: "center",
   gap: "3.125rem",
   alignSelf: "stretch",
   listStyleType: "none",
 });
 
 export const menuItem = style({
-  fontSize: "24px",
+  fontSize: "1.5rem",
   fontStyle: "normal",
   fontWeight: "400",
   lineHeight: "normal",
+  display: "inline-block",
+  position: "relative",
+  textAlign: "center",
+  textDecoration: "none",
 });
+
+
+export const orgText = style({
+  position: "absolute",
+  right: "4rem",
+  bottom: "0.95rem",
+  textAlign: "center",
+  fontSize: "1.25rem",
+  fontStyle: "normal",
+  fontWeight: "400",
+  lineHeight: "normal",
+  marginLeft: "0rem",
+});
+
+export const divider = style({
+  display: "inline-block",
+  height: "50%",
+  borderLeft: "1px solid",
+  margin: "0rem",
+  verticalAlign: "midddle",
+});
+
